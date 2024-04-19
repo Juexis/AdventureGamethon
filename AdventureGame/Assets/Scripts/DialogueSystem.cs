fbf4ce7ff8cbdb89367ac09e046d7baa756d09d5
CoderNoot <andyzheng812@aol.com>
2024-04-19 14:16:14 -0400

    Resolve Error

--- AdventureGame/Assets/Scripts/DialogueSystem.cs
+++ AdventureGame/Assets/Scripts/DialogueSystem.cs
@@ -8,16 +8,17 @@ using UnityEngine.Analytics;
 public class DialogueSystem : MonoBehaviour
 {
     public TextMeshProUGUI textbox;
-    public Interactable InteractScript;
+    public BInteractable InteractScript;
     public PlayerMovement Pmove;
     string dialogue;
     string objectName;
     [SerializeField] private Health playerHealth;
-
+    string status;
 
     private void Start()
     {
         //InteractScript = gameObject.GetComponent<Interactable>();
+        textbox.text = string.Empty;
     }
     private void Update()
     {
@@ -38,14 +39,44 @@ public class DialogueSystem : MonoBehaviour
 
         switch (objectName)
         {
+            case "InteractableRange":
+                dialogue = "Press E to interact";
+                Debug.Log("Interactable");
+                UpdateText(dialogue);
+                break;
             case "Bed":
                 Pmove.InDialogue();
                 dialogue = "You are not tired...";
+                Debug.Log("you are not tired");
+                UpdateText(dialogue);
+                break;
+            case "Door":
+                Pmove.InDialogue();
+                dialogue = "You don't feel like leaving yet.";
+                Debug.Log("door");
                 UpdateText(dialogue);
                 break;
         }
     }
 
+    public void Inrangenow(string dialogHitbox)
+    {
+        status = dialogHitbox;
+        switch (status)
+        {
+            case "InteractableRange":
+                dialogue = "Press E to interact";
+                Debug.Log("interactable");
+                UpdateText(dialogue);
+                break;
+            default:
+                dialogue = string.Empty;
+                UpdateText(dialogue);
+                break;
+
+        }
 
 
-}
+    }
+
+    }
