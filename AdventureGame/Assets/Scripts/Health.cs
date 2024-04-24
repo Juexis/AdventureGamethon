30eeeabf5ca350d74792bd99fe183d8692977546
CoderNoot <andyzheng812@aol.com>
2024-04-24 13:48:55 -0400

    PushBack and Iframes

--- AdventureGame/Assets/Scripts/Health.cs
+++ AdventureGame/Assets/Scripts/Health.cs
@@ -7,9 +7,16 @@ public class Health : MonoBehaviour
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
 
+    //Iframes
+    [SerializeField] private float invulnerabilityTime;
+    [SerializeField] private float numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
+
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage (float _damage)
@@ -17,11 +24,7 @@ public class Health : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
         if (currentHealth > 0)
         {
-
-        }
-        else
-        {
-
+            StartCoroutine(iframes());
         }
     }
     private void Update()
@@ -37,6 +40,19 @@ public class Health : MonoBehaviour
     public void deleteHealth( float _value)
     {
         TakeDamage(1);
+    }
+
+    private IEnumerator iframes()
+    {
+        Physics2D.IgnoreLayerCollision(6, 10, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(0.6603f, 0.6603f, 0.6603f, 0.5f);
+            yield return new WaitForSeconds(invulnerabilityTime / (numberOfFlashes *2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(invulnerabilityTime / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(6, 10, false);
     }
 }
 
--- AdventureGame/Assets/Scripts/SlimeMove.cs
+++ AdventureGame/Assets/Scripts/SlimeMove.cs
@@ -6,21 +6,23 @@ public class SlimeMove : MonoBehaviour
 {
     // Start is called before the first frame update
     float speed = 1.04F;
-
+    private PlayerMovement player;
     Tracker myTracker;
+    float pushRadius = 15f;
 
     // Use this for initialization
     void Start()
     {
         myTracker = new Tracker(gameObject, "Player");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 direction = myTracker.GetDirection();
-
         transform.Translate(direction.x * Time.deltaTime * speed, direction.y * Time.deltaTime * speed, 0);
+        
     }
 
     [SerializeField] private float healthValue;
@@ -30,11 +32,34 @@ public class SlimeMove : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<Health>().deleteHealth(healthValue);
-
+            DoPush();
 
         }
     }
 
+    void DoPush()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, pushRadius);
+
+        foreach (Collider2D pushedObject in colliders)
+        {
+            if (pushedObject.CompareTag("Player"))
+            {
+                Rigidbody2D pushedBody = pushedObject.GetComponent<Rigidbody2D>();
+
+                // Get direction from your postion toward the object you wish to push
+                var direction = pushedBody.transform.position - transform.position;
+
+                // Normalization is important, to have constant unit!
+                pushedBody.AddForce(direction.normalized * 2500, ForceMode2D.Force);
+
+            }
+        }
 
 
-}
+
+
+
+    }
+
+    }
